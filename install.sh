57dbdcc4e112f0f6d875b7064c3c93a4e0b6545a
d.stroch <d.stroch@gmail.com>
2022-02-18 14:33:43 +0100

    Add install and uninstall basic setup of mac

---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/bin/zsh
+
+# brew installation
+# https://github.com/homebrew/install#uninstall-homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "[INSTALLING] brew ✅ "
+sleep 1
+
+# Oh-My-Zsh installation
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "[INSTALLING] Oh-My-Zsh ✅ "
+sleep 1
+cd
+
+# install apps via brew
+cask_apps=('keepassxc' 'vscodium' 'slack' 'firefox' 'microsoft-remote-desktop')
+for app in "${cask_apps[@]}" ;do
+    brew install --cask $app
+    echo "[INSTALLING] $app ✅ "
+    sleep 1
+
+# Create auxiliary dirs or files
+mkdir ~/RSBP/
+echo "[INFO] create RSBP dir ✅ "
+
+# Show hidden files
+defaults write com.apple.Finder AppleShowAllFiles true
+killall Finder
+sleep 1
+echo "[SETUP] show hidden files ✅ "
+
+# Setup git
+git config --global init.defaultBranch main
+git config --global merge.conflictstyle diff3
+git config --global core.editor vim
+git config --global color.ui True
+git config --global user.email d.stroch@gmail.com
+git config --global user.name d.stroch
+if ! [ -d $HOME/.config/git ]
+then
+    mkdir -p $HOME/.config/git
+    echo "[INFO] create $HOME/.config/git ✅ "
+    cp $PWD/ignore $HOME/.config/git/
+    echo "[INFO] ignore copied to $HOME/.config/git ✅ "
+else
+    cp $PWD/ignore $HOME/.config/git/
+    echo "[INFO] ignore copied to $HOME/.config/git ✅ "
+fi
+echo "[SETUP] git ✅ "
+
+# Setup Oh My Zsh
+cp $PWD/custom_kphoen.zsh-theme $HOME/.oh-my-zsh/themes/kphoen.zsh-theme
+cp $PWD/.zshrc $HOME/
+source $HOME/.zshrc
+# echo "asdasd" >> ~/.zshrc
+echo "[SETUP] Oh-My-Zsh ✅ "
---
+++ uninstall.sh
@@ -0,0 +1,40 @@
+#!/bin/zsh
+
+#brew uninstall
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+echo "[UNINSTALLING] brew ✅ "
+
+# Oh-My-Zsh uninstall
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/uninstall.sh)"
+echo "[UNINSTALLING] Oh-My-Zsh ✅ "
+sleep 1
+cd
+
+# install apps via brew
+cask_apps=('keepassxc' 'vscodium' 'slack' 'firefox' 'microsoft-remote-desktop')
+for app in "${cask_apps[@]}" ;do
+    brew uninstall --cask $app
+    echo "[UNINSTALLING] $app ✅ "
+    sleep 1
+
+# Remove auxiliary dirs or files
+rm -r ~/RSBP/
+echo "[INFO] RSBP dir removed ✅ "
+
+# Hide hidden files
+defaults write com.apple.Finder AppleShowAllFiles false
+killall Finder
+sleep 1
+echo "[SETUP] hide hidden files ✅ "
+
+# Setup git
+if [ -f $HOME/.gitconfig ]
+then
+    rm $HOME/.gitconfig
+fi
+
+if [ -d $HOME/.config/git ]
+then
+    rm -r $HOME/.config/git
+    echo "[REMOVE] $HOME/.config/git ✅ "
+fi
